b122e60d3b600f87ca8cbf17d41ee60f1d568d27
satz-create <thesathishkumar05@gmail.com>
2022-11-30 14:59:48 +0530

    final code

---
+++ src/test/java/org/text/Sathish.java
@@ -0,0 +1,18 @@
+package org.text;
+
+public class Sathish {
+	
+	private void method1() {
+		// TODO Auto-generated method stub
+System.out.println("abcd");
+	}
+	private void method2() {
+		// TODO Auto-generated method stub
+System.out.println("method2");
+	}
+	private void method3() {
+		// TODO Auto-generated method stub
+System.out.println("method3");
+	}
+
+}
